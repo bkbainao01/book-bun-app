ec02793211ede5f564a5f9db0da9d446bdb558d1
Sirimanee Pimkaew <sirimanee@imaxxsol.com>
2025-10-03 13:40:42 +0700

    update about environment of prod

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-const baseApiUrl = import.meta.env.VITE_API_URL || 'http://localhost:5175'
+const baseApiUrl = import.meta.env.VITE_BASE_API_URL || 'http://localhost:5175'
 export const config = {
     baseUrl: baseApiUrl
 }
